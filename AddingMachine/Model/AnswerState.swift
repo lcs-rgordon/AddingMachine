c7da272192826909446fdba8f17381ff481f305e
Russell Gordon <rgordon@lcs.on.ca>
2024-01-10 14:03:55 -0500

    Checking the answer given against the correct answer and setting the appropriate result (incorrect or correct).

---
+++ AddingMachine/Model/AnswerState.swift
@@ -0,0 +1,14 @@
+//
+//  AnswerState.swift
+//  AddingMachine
+//
+//  Created by Russell Gordon on 2024-01-10.
+//
+
+import Foundation
+
+enum AnswerState: String {
+    case noInputGiven = ""
+    case incorrect = "❌"
+    case correct = "✅"
+}
--- AddingMachine/Views/QuizView.swift
+++ AddingMachine/Views/QuizView.swift
@@ -9,10 +9,13 @@ import SwiftUI
 
 struct QuizView: View {
     
+    // MARK: Stored properties
     @State var firstNumber = Int.random(in: 1...25)
     @State var secondNumber = Int.random(in: 1...25)
     @State var answerGiven = ""
+    @State var result: AnswerState = .noInputGiven
     
+    // MARK: Computed properties
     var body: some View {
         HStack {
             
@@ -37,12 +40,40 @@ struct QuizView: View {
                 TextField("Answer", text: $answerGiven)
                     .font(.custom("Helvetica", size: 96.0))
                     .multilineTextAlignment(.trailing)
+                
+                HStack {
+                    Spacer()
+                    
+                    Button(action: {
+                        // Check answer
+                    }, label: {
+                        Text("Submit")
+                    })
+                }
             }
             
             // List of past questions tried
             Text("History")
         }
         .frame(width: 800, height: 400)
+    }
+    
+    // MARK: Functions
+    func checkAnswer() {
+        let correctAnswer = firstNumber + secondNumber
+        
+        // Try to make the answer given into an integer
+        guard let answerGivenAsInteger = Int(answerGiven) else {
+            // Can't make input into an integer
+            return
+        }
+        
+        if answerGivenAsInteger == correctAnswer {
+            // Tell the user they got it right
+            result = .correct
+        } else {
+            result = .incorrect
+        }
     }
 }
 
