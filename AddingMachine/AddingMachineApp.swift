a0e99b73f83f39af7a754c39e3d744038f14e5ac
Russell Gordon <rgordon@lcs.on.ca>
2024-01-10 13:26:45 -0500

    Initial Commit

---
+++ AddingMachine/AddingMachineApp.swift
@@ -0,0 +1,17 @@
+//
+//  AddingMachineApp.swift
+//  AddingMachine
+//
+//  Created by Russell Gordon on 2024-01-10.
+//
+
+import SwiftUI
+
+@main
+struct AddingMachineApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
