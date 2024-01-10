15f4a9d2c4cd603d88a3a7501767b8f753744f57
Russell Gordon <rgordon@lcs.on.ca>
2024-01-10 13:38:28 -0500

    Organized project files and started to make main interface.

---
+++ AddingMachine/Views/QuizView.swift
@@ -0,0 +1,23 @@
+//
+//  QuizView.swift
+//  AddingMachine
+//
+//  Created by Russell Gordon on 2024-01-10.
+//
+
+import SwiftUI
+
+struct QuizView: View {
+    var body: some View {
+        HStack {
+            Text("Quiz interface")
+            
+            Text("History")
+        }
+        .frame(width: 800, height: 400)
+    }
+}
+
+#Preview {
+    QuizView()
+}
--- AddingMachineApp.swift
+++ AddingMachineApp.swift
@@ -11,7 +11,7 @@ import SwiftUI
 struct AddingMachineApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            QuizView()
         }
     }
 }
